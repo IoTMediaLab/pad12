5bee5ae9d52da1556db49cde62fca10a09c3c534
isi <isi.yma@gmail.com>
2018-10-04 14:38:35 +0900

    tes

--- pad12key.py
+++ pad12key.py
@@ -24,10 +24,10 @@ def GPIO_INIT():
     ''' GPIO INIT '''
     GPIO.setmode(GPIO.BOARD) #GPIOへアクセスする番号を ボードのPIN番号とする
 
-    chan_list = [C1,C2,C3]
+    chan_list = [R1,R2,R3,R4]
     GPIO.setup(chan_list,GPIO.OUT)
 
-    chan_list = [R1,R2,R3,R4]
+    chan_list = [C1,C2,C3]
     GPIO.setup(chan_list,GPIO.IN)
 
 def get():
@@ -36,11 +36,11 @@ def get():
     r_list = [R1,R2,R3,R4]
     #GPIO.output(chan_list,GPIO.HIGH)
 
-    for i,c in enumerate(c_list):
-        GPIO.output(c,GPIO.HIGH)
-        for j,r in enumerate(r_list):
-            print('C'+str(j+1),GPIO.input(r),'  ',end='')
-        GPIO.output(c,GPIO.LOW)
+    for i,r in enumerate(r_list):
+        GPIO.output(r,GPIO.HIGH)
+        for j,c in enumerate(c_list):
+            print('R'+str(j+1),GPIO.input(c),'  ',end='')
+        GPIO.output(r,GPIO.LOW)
         print(' ')
 
 
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     ''' テスト用メイン関数 '''
     GPIO_INIT()
 
-    GPIO.output(C1,GPIO.HIGH)
+    GPIO.output(R1,GPIO.HIGH)
 
     while(1):
-        print(GPIO.input(R1))
+        print(GPIO.input(C1))
 
     GPIO.cleanup()
